d1b604ea3a333f522edc2f840607ece8390e2d35
edisonzhou <edisonchou@xique.com>
2022-09-02 11:50:21 +0800

    增加HealthController用于HealthCheck

---
+++ src/EDT.MSA.Order.API/Controllers/HealthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EDT.MSA.Ordering.API.Controllers
+{
+    [Route("api/Health")]
+    [ApiController]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok($"ok");
+    }
+}
---
+++ src/EDT.MSA.Stock.API/Controllers/HealthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EDT.MSA.Stocking.API.Controllers
+{
+    [Route("api/Health")]
+    [ApiController]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok($"ok");
+    }
+}
