3125a83871eb62ff248ba8de1a6161442c3acd6a
edisonzhou <edisonchou@xique.com>
2022-09-02 13:42:18 +0800

    添加GetNode接口用于负载均衡示例

--- src/EDT.MSA.Stock.API/Controllers/HealthController.cs
+++ src/EDT.MSA.Stock.API/Controllers/HealthController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Sockets;
 
 namespace EDT.MSA.Stocking.API.Controllers
 {
@@ -9,5 +11,23 @@ namespace EDT.MSA.Stocking.API.Controllers
     {
         [HttpGet]
         public IActionResult Get() => Ok($"ok");
+
+        [HttpGet("node")]
+        public IActionResult GetNodeInfo()
+        {
+            var result = string.Empty;
+            var HostName = Dns.GetHostName();
+            var IpEntry = Dns.GetHostEntry(HostName);
+            for (int i = 0; i < IpEntry.AddressList.Length; i++)
+            {
+                // 从IP地址列表中筛选出IPv4类型的IP地址
+                if (IpEntry.AddressList[i].AddressFamily == AddressFamily.InterNetwork)
+                {
+                    result = IpEntry.AddressList[i].ToString();
+                }
+            }
+
+            return Ok($"Current Node: {result}");
+        }
     }
 }
